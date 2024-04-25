798cf6fef658921ea1a6ea1d739463cd25887def
parthiban <55436038+parthibanvthink@users.noreply.github.com>
2024-04-25 07:50:08 +0530

    content updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   resources :logins
+  # resources :logins do
+  #   collection do
+  #     get 'index', to: 'logins#index'
+  #     post 'create', to: 'logins#create'
+  #     # get 'show', to: 'logins#show'
+  #     # get 'logins/show', to: 'logins#show', as: 'show_login'
+  #     # get 'test', to: 'logins#test', as: 'test_login'
+  #   end
+  # end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
